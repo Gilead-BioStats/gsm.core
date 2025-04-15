35dc3280e4ae073b2e90c13e72fdca2f3b0f4411
Laura Maxwell <laura.maxwell@atorusresearch.com>
2025-04-15 16:34:10 -0400

    don't init git in function- git repo must be setup prior to running this function

--- R/util-init_gsm_package.R
+++ R/util-init_gsm_package.R
@@ -1,22 +1,21 @@
 #' Initialize gsm Extension package
 #'
 #' @param package_dir path to package directory
-#' @param init_git boolean argument declaring whether or not to initialize this directory as a git repo. Default is `FALSE`.
 #' @param description_fields list of description fields, passed to [usethis::create_package()]. Default is `list()`.
 #' @param include_workflow_dir boolean argument declaring whether or not to include the `inst/workflow` directory in the root of the package. Default is `TRUE`.
 #'
 #' @export
 init_gsm_package <- function(package_dir,
-                             init_git = FALSE,
                              description_fields = list(),
                              include_workflow_dir = TRUE) {
+  if(!dir.exists(package_dir)) {
+    dir.create(package_dir)
+    init_git = TRUE
+  }
   usethis::create_package(package_dir,
                           open = F,
                           fields = description_fields)
   withr::with_dir(package_dir, {
-  if(init_git) {
-    usethis::use_git()
-  }
   usethis::use_pkgdown_github_pages()
   usethis::use_testthat()
   usethis::use_github_action("check-standard")
