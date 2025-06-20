f8f9dd23570cfffc8601848185cbf595c25b9365
Jon Harmon <jon.harmon@atorusresearch.com>
2025-06-20 10:29:56 -0500

    Document pipe.

--- R/util-pipe.R
+++ R/util-pipe.R
@@ -1,5 +1,14 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
 #' @name %>%
-#' @noRd
+#' @rdname pipe
+#' @keywords internal
 #' @export
-#' @importFrom dplyr %>%
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
 NULL
